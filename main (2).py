564c08c056d8fc1b3244c479b1b38933c38cc288
silverballz <anuragmessi5@gmail.com>
2024-06-23 11:38:44 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,24 @@
+print("Welcome to LULU Island.")
+print("Your mission is to find the LULU Box.") 
+
+
+#Write your code below this line 👇
+
+
+choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
+if choice1 == "left":
+  choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swoosh" to NARUTO-RUN across. \n').lower()
+  if choice2 == "wait":
+    choice3 = input("You arrive at the other end unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+    if choice3 == "red":
+      print("It's a room full of fire. LULULULU! Game Over.")
+    elif choice3 == "yellow":
+      print("You found the treasure! LULULULU! You Win!")
+    elif choice3 == "blue":
+      print("You enter a room of beasts. LULULULU! Game Over.")
+    else:
+      print("You chose a door that doesn't exist. IDIOT! Game Over.")
+  else:
+    print("You get attacked by an angry UMAlulu. Game Over.")
+else:
+  print("You fell into a hole. LULULULU! Game Over.")
